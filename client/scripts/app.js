48e509e9491cfbeaa00e50646b7163895cef499d
Tim Dang <timothy.dang@gmail.com>
2016-01-25 15:26:51 -0800

    Setup work environment. Completed some tests like sending.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,14 @@
-// YOUR CODE HERE:
+var app = {};
 
+app.init = function () {};
+
+app.send = function (message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data){ console.log('chatterbox: Message sent. Data: ', data); },
+    error: function(data) { console.log('chatterbox: Failed to send message. Error: ', data); }
+  });
+};
