7ba8475dd24f7c25f4afeac244608aba6c786153
Tim Dang <timothy.dang@gmail.com>
2016-01-25 17:03:55 -0800

    Added ability to load messages by manually calling app.init()

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,46 @@
 var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox';
 
-app.init = function () {};
+app.init = function() {
+  app.fetch(app.populate);
+};
 
-app.send = function (message) {
+app.populate = function(data){
+  var retrievedData = data.results;
+    var $main = $('#main');
+  for (var i = 0; i < retrievedData.length; i++) {
+    $main.append('<div class="' + retrievedData[i].roomname +
+      '">Name: ' + retrievedData[i].name +
+      '\nMessage: ' + retrievedData[i].message + '</div>');
+  }
+};
+
+app.send = function(message) {
   $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
-    success: function(data){ console.log('chatterbox: Message sent. Data: ', data); },
-    error: function(data) { console.log('chatterbox: Failed to send message. Error: ', data); }
+    success: function(data) {
+      console.log('chatterbox: Message sent. Data: ', data);
+    },
+    error: function(data) {
+      console.log('chatterbox: Failed to send message. Error: ', data);
+    }
+  });
+};
+
+app.fetch = function(callback) {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Fetched. Data: ', data);
+      callback(data);
+    },
+    error: function() {
+      console.log('chatterbox: Failed to fetch data. Error');
+    }
   });
 };
